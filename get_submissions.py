ddfceeb1911e9dbf5ce5ff9c83bc47316dcaa287
Zaya Battogtokh <zaya.batt@gmail.com>
2019-12-01 02:13:06 -0800

    finish get_submissions

--- get_submissions.py
+++ get_submissions.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import datetime as dt
 from psaw import PushshiftAPI
-import praw 
+import praw
 import pickle
 
 reddit = praw.Reddit(client_id='sIF2FculBPoFMg',
@@ -22,4 +22,39 @@ kept = []
 count = 0
 
 
+for idx, obj in enumerate(objects):
+    if idx % 100 == 0:
+        print (idx)
+    title = obj.title.lower()
+    text = obj.selftext
+    url = obj.url
+    flair = obj.link_flair_text
 
+
+    if obj.author == None:
+        continue
+
+    try:
+        test = obj.author.created_utc
+    except:
+        pass
+
+    if "request]" not in title or text == "[removed]" or text == "[deleted]":
+        continue
+
+    if flair == "No Longer Needed":
+        continue
+
+    if flair == "Fulfilled":
+        count += 1
+
+    kept.append(obj)
+
+
+
+print (len(objects), count, len(kept))
+
+print ("percentage of fulfilled," count / len(kept))
+
+with open('submissions.pkl','wb') as f:
+    pickle.dump(kept, f)
