05c6106953faecf2f4c0aebb27b3eb7d6b5ba990
Zaya <bilguunzaya_batt@instance-1.us-central1-a.c.cs221-final-260622.internal>
2019-12-01 12:35:23 +0000

    proper sub.pkl file

--- get_submissions.py
+++ get_submissions.py
@@ -4,16 +4,18 @@ from psaw import PushshiftAPI
 import praw
 import pickle
 
-reddit = praw.Reddit(client_id='sIF2FculBPoFMg',
-                             client_secret='ONlvY9ziXYVBhTeMvp1y4yP4Fg4',
-                                                  user_agent='billyisnotthegoat')
+reddit = praw.Reddit(client_id='sIF2FculBPoFMg',client_secret='ONlvY9ziXYVBhTeMvp1y4yP4Fg4',user_agent='billyisnotthegoat')
 
 api = PushshiftAPI(reddit)
+
+print("Pushshift API Initialized...")
 
 before_date = int(dt.datetime(2019, 11, 1).timestamp())
 after_date = int(dt.datetime(2017, 3, 1).timestamp())
 
 gen = api.search_submissions( after=after_date, before=before_date, subreddit='Random_Acts_Of_Pizza')
+
+print("Now enumerating over objects...")
 
 objects = list(gen)
 
@@ -37,7 +39,7 @@ for idx, obj in enumerate(objects):
     try:
         test = obj.author.created_utc
     except:
-        pass
+        continue
 
     if "request]" not in title or text == "[removed]" or text == "[deleted]":
         continue
@@ -54,7 +56,7 @@ for idx, obj in enumerate(objects):
 
 print (len(objects), count, len(kept))
 
-print ("percentage of fulfilled," count / len(kept))
+print ("percentage of fulfilled:",  count / len(kept))
 
 with open('submissions.pkl','wb') as f:
     pickle.dump(kept, f)
